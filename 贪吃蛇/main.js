46709dc85369de75bbe1b6ecf79c65ad630a6260
Vivipure <vivipureheart@gmail.com>
2019-02-22 23:40:15 +0800

    add demonstrate png

--- 贪吃蛇/main.js
+++ 贪吃蛇/main.js
@@ -9,13 +9,15 @@ var log = function () {
 			setInterval(game,1000/15)
 		}	
 		var xv=yv=0,//x,y 轴速度
-		px=py=10, //snake 位置
-		gs=tc=20, 
+		px=py=10, //
+		gs=tc=20, //
 		ax=ay=15,  
 		trail=[],tail=5
 		function game () {
 			px +=xv
 			py +=yv
+
+			//出界 检测
 			if(px < 0) {
 				px = tc - 1
 			}
@@ -31,24 +33,31 @@ var log = function () {
 			//幕布
 			ctx.fillStyle = 'black'
 			ctx.fillRect(0,0,canv.width,canv.height)
-
+			// tail 为蛇的长度 
 			ctx.fillStyle = 'lime'
 			for (var i = 0;i < trail.length;i++) {
+				// snake
+				//first  (200,200,18,18)
 				ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2)
+
+				// 设定 初始长度为5 若反向 则重置
 				if(trail[i].x == px && trail[i].y == py) {
 					tail = 5;
 				}
 			}
 			trail.push({x:px,y:py})
+            
+            //更新位置
 			while(trail.length>tail){
 				trail.shift()
 			}
+			//snake block collision  长度+1  再随机ax,ay   变换block 位置
 			if (ax == px && ay == py) {
 				tail++
 				ax=Math.floor(Math.random()*tc)
 				ay=Math.floor(Math.random()*tc)
 			}
-			
+			//draw block
 			ctx.fillStyle = 'red'
 			ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2)
 		}
